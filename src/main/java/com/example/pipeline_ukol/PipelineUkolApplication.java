ec18c738ad367975beb293546a68a3822e298b0c
Pepam01 <jmiv004@gmail.com>
2023-02-21 14:24:49 +0100

    frst

---
+++ src/main/java/com/example/pipeline_ukol/PipelineUkolApplication.java
@@ -0,0 +1,13 @@
+package com.example.pipeline_ukol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PipelineUkolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PipelineUkolApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/pipeline_ukol/PipelineUkolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pipeline_ukol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PipelineUkolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
